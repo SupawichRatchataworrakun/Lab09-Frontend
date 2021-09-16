9368a0b3ae6f5f4ae9c70b21472923919e6b7832
SupawichRatchataworrakun <supawich_r@elearning.cmu.ac.th>
2021-09-16 14:56:56 +0700

    6.1

--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -38,12 +38,9 @@
 </template>
 <script>
 import EventService from '@/services/EventService.js'
-import BaseInput from '@/components/BaseInput'
 export default {
   inject: ['GStore'],
-  components: {
-    BaseInput
-  },
+  
   data() {
     return {
       event: {
